9c63d114d6eed706d91f1bf348ca2fee4def3ba1
Insculpo <tkaufeld@ucsc.edu>
2018-11-21 17:58:44 -0800

    Nashid is Born!
    Rise by mantid queen!

---
+++ BoxingMantidPitchProto/Assets/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	Vector2 rotation = new Vector2 (0, 0);
+	public float camSpeed = 10f;
+	public float rotSpeed = 10f;
+	Transform cameracontrols;
+	public GameObject player;
+	private Vector3 off;
+
+	// Use this for initialization
+	void Start () {
+		cameracontrols = this.GetComponent<Transform>();
+		off = transform.position - player.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Lines 22-24 are ripped from an online post for making a simple camera.
+		rotation.y += Input.GetAxis ("Mouse X");
+		rotation.x -= Input.GetAxis ("Mouse Y");
+	}
+	void LateUpdate() {
+		cameracontrols.transform.position = player.transform.position + off;
+	}
+}
---
+++ BoxingMantidPitchProto/Assets/NancidController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NancidController : MonoBehaviour {
+
+	public Transform mantisMover; 
+	public Rigidbody mantisPhysics;
+	public float thrust = 5f;
+	public float jump = 2f;
+	Vector3 jumpForce;
+
+	// Use this for initialization
+	void Start () {
+		mantisPhysics.GetComponent<Rigidbody> ();
+		mantisMover = GetComponent<Transform> ();
+		jumpForce = new Vector3 (0, jump, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			
+			float horizontal = Input.GetAxis ("Horizontal");
+			float vertical = Input.GetAxis ("Vertical");
+			Vector3 move = new Vector3 (horizontal * thrust, jump, vertical * thrust);
+			mantisPhysics.AddForce (move);
+		} else {
+			float horizontal = Input.GetAxis ("Horizontal");
+			float vertical = Input.GetAxis ("Vertical");
+			Vector3 move = new Vector3 (horizontal * thrust, 0, vertical * thrust);
+			mantisPhysics.AddForce (move);
+		//	mantisPhysics.AddForce (jumpDirection.eulerAngles * thrust);
+		}
+	}
+}
