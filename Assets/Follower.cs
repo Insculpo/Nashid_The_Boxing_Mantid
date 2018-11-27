e0b528b277351d214c61c6dd09bef7a63bd232ad
Dimitri Kusnetsov <Insculpo@mikes-iMac.local>
2018-11-26 19:15:34 -0800

    nantis mantis time

---
+++ Assets/Follower.cs
@@ -0,0 +1,71 @@
+﻿//Source taken from: https://github.com/Acacia-Developer/Acacia-Developer-Tutorials/tree/master/%5BUnity%20C%23%20%5D%20First%20Person%20Controller%20Series/E01%20Basic%20FPS%20Controller%20and%20Jumping
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    public GameObject target;
+    Vector2 rotation = new Vector2(0, 0);
+    [SerializeField] float speed;
+    public float reach = 4f;
+    public float punch = 15f;
+    public float holdP = 10f;
+    private float xAxisClamp;
+    MantisController multiplier;
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClamp = 0.0f;
+        multiplier = target.GetComponent<MantisController>();
+    }
+
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        this.transform.position = target.transform.position;
+        CameraRotation();
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Push();
+        }
+
+    }
+
+    private void CameraRotation()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x -= Input.GetAxis("Mouse Y");
+        transform.eulerAngles = (Vector2)rotation * speed;
+
+    }
+
+    void Push()
+    {
+        RaycastHit rays;
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out rays, punch))
+        {
+            Rigidbody array = rays.transform.GetComponent<Rigidbody>();
+            if (array != null)
+            {
+                float punchHold = 0;
+                while (punchHold < holdP)
+                {
+                    Vector3 forcer = Camera.main.transform.forward * reach;
+                    array.AddForce(forcer);
+                    punchHold++;
+                }
+            }
+        }
+    }
+
+    
+}
---
+++ Assets/MantisController.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MantisController : MonoBehaviour {
+
+    Rigidbody mantisControls;
+    CharacterController mantisChar;
+    [SerializeField] public float jumpForce = 30f;
+    [SerializeField] public float flutterPower = 100f;
+    [SerializeField] public float speed = 0.1f;
+    [SerializeField] public float strafe = 1.2f;
+    [SerializeField] public float Delays = 100f;
+    [SerializeField] public float Recovery = 1f;
+    [SerializeField] public float RecoveryF = 1f;
+
+    float InitialDelays;
+    [SerializeField] public float flutterState = 0f;
+    [SerializeField] public Slider valM;
+
+    bool isDelay;
+    bool isJumping;
+
+    // Use this for initialization
+    void Start () {
+        InitialDelays = Delays;
+        isDelay = false;
+        isJumping = false;
+        mantisControls = this.GetComponent<Rigidbody>();
+        mantisChar = this.GetComponent<CharacterController>();
+        Camera.main.transform.position = mantisChar.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        float forward = Input.GetAxis("Vertical") * speed;
+        float sideways = Input.GetAxis("Horizontal") * speed;
+
+        forward *= Time.deltaTime;
+        sideways *= Time.deltaTime;
+
+        localize(forward,sideways);
+        flutterCheck();
+
+        valM.value = flutterState;
+       
+        //mantisControls.MoveRotation(Camera.main.transform.rotation);
+
+    }
+
+    void flutterCheck(){
+        if (Input.GetAxis("Jump").Equals(1) && isDelay == false)
+        {
+            mantisControls.AddForce(0, jumpForce, 0);
+            flutterState++;
+            isJumping = true;
+        }else{
+            isJumping = false;
+        }
+        if (isJumping == false && flutterState > 0)
+        {
+            flutterState -= RecoveryF;
+        }
+        if (flutterState > flutterPower)
+        {
+            isDelay = true;
+        }
+
+        if (isDelay == true)
+        {
+            Delays -= Recovery;
+            if (Delays <= 0)
+            {
+                flutterState = 0;
+                isDelay = false;
+                Delays = InitialDelays;
+            }
+        }
+
+    }
+
+    void localize(float x, float z) {
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            Debug.Log("Left");
+
+            Vector3 rot = new Vector3(x, 0, z);
+            Vector3 dirs = Quaternion.Euler(0, -90, 0) * rot;
+            Vector3 moveDir = Camera.main.transform.TransformDirection(dirs);
+            moveDir *= (z * strafe);
+            transform.Translate(moveDir);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Debug.Log("Right");
+            Vector3 rot = new Vector3(x, 0, z);
+            Vector3 dirs = Quaternion.Euler(0, 90, 0) * rot;
+            Vector3 moveDir = Camera.main.transform.TransformDirection(dirs);
+            moveDir *= (z * strafe);
+            transform.Translate(moveDir);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Debug.Log("Forward");
+
+            Vector3 rot = new Vector3(x, 0, z);
+            Vector3 dirs = Quaternion.Euler(0, -90, 0) * rot;
+            Vector3 moveDir = Camera.main.transform.TransformDirection(dirs);
+            moveDir *= x;
+            transform.Translate(moveDir);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Debug.Log("Backward");
+
+            Vector3 rot = new Vector3(x, 0, z);
+            Vector3 dirs = Quaternion.Euler(0, 90, 0) * rot;
+            Vector3 moveDir = Camera.main.transform.TransformDirection(dirs);
+            moveDir *= x;
+            transform.Translate(moveDir);
+        }
+    }
+}
